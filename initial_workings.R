cb00f7f7ee029b3486a2a84f557208728f1079bb
Will Sutton <sutton_999@hotmail.com>
2020-12-23 16:57:11 +0000

    testing out slide deck frameworks

---
+++ initial_workings.R
@@ -0,0 +1,13 @@
+
+### Reveal.js slides
+# Link: https://github.com/rstudio/revealjs
+#install.packages("revealjs")
+library(revealjs)
+
+
+### Xaringan
+# Link: https://arm.rbind.io/slides/xaringan.html#1
+# Install Link: https://arm.rbind.io/prework/packages/
+arm_from_cran <- c("flexdashboard", "learnr", "bookdown",
+                   "officer", "rticles", "webshot",
+                   "tidyverse", "remotes", "babynames", "magick")
